4333ecf4104cd68d964f41298077e5653800b8e5
papi18011998 <papaibrahima98@gmail.com>
2021-11-16 11:38:29 +0000

    mise en place du boutton scroll to top

---
+++ js/partenariat.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+myBtn.onclick = function(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+}
